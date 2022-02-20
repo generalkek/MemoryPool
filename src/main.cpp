3f21bea9a835d5cad04b9c978861ce471460ea05
MrNemo <jenya21998@gmail.com>
2022-02-20 22:49:20 +0200

    Added basic timing test

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,58 @@
 #include "sbp.h"
 
 #include <iostream>
+#include <ctime>
 
 struct A
 {
-	int* a;
-	A()
-	{
-		a = new int;
-	}
-	~A() 
-	{
-		std::cout << "~A()\n";
-		delete a;
-	};
+	char data[50];
 };
+
+struct Timer
+{
+	Timer()
+	{
+		start = std::clock();
+	}
+
+	double getDelt()
+	{
+		clock_t temp = std::clock();
+		double res = (temp - start) / static_cast<double>(CLOCKS_PER_SEC);
+		start = temp;
+		return res;
+	}
+private:
+	clock_t start;
+};
+
+void timingTest()
+{
+	const unsigned int iteration = 100000;
+
+	Timer t;
+	A* ptr;
+	for (int i = 0; i < iteration; i++)
+	{
+		ptr = (A*)std::malloc(sizeof(A));
+		std::free(ptr);
+	}
+
+	std::cout << t.getDelt() << "\n";
+
+	for (int i = 0; i < iteration; i++)
+	{
+		ptr = new A;
+		delete ptr;
+	}
+
+	std::cout << t.getDelt() << "\n";
+
+}
 
 int main(int argc, char** arvg)
 {
-	/*sbp::StackBasedPool pool;
+	timingTest();
 
-	void* ptr1 = pool.malloc(10);
-	void* ptr2 = pool.malloc(20);
-	void* ptr3 = pool.malloc(40);
-	void* ptr4 = pool.malloc(80);
-
-	pool.free();
-	pool.free();
-	pool.free();
-	pool.free();*/
-
-	int* pi = new int(32);
 	return 0;
 }
